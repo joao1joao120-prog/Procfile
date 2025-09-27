8fffef6f2f674b47a00b0b4f8300b46ebcff5659
joao1joao120-prog <joao1joao120@gmail.com>
2025-09-26 22:47:49 -0300

    Update app.py

--- app.py
+++ app.py
@@ -7,11 +7,11 @@ app = Flask(__name__)
 # 2. Define a rota principal
 @app.route('/')
 def meu_servico_principal():
-    # A linha 'return' deve ter indentação (espaços)
+    # Deve ter 4 espaços no início
     return "O código Python finalmente migrou e está online!"
 
 # 3. Bloco de inicialização com compatibilidade de servidor
 if __name__ == '__main__':
-    # As próximas duas linhas devem ter indentação (espaços)
+    # As próximas duas linhas devem ter 4 espaços no início
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
